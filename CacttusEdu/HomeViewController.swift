5f1b8849d42faaf13458ee7bcc5d0e95f34d890a
Gent Berani <gentberani@gmail.com>
2018-02-19 01:09:02 +0100

    add: first&second week

---
+++ CacttusEdu/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  CacttusEdu
+//
+//  Created by Gent Berani on 2/14/18.
+//  Copyright © 2018 CacttusEdu. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var firstLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        firstLabel.text = "Hello World"
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let secondController = segue.destination as? SecondViewController
+        secondController?.test = "Hello from HomeViewController"
+    }
+    
+    @IBAction func topButtonPressed(_ sender: UIButton) {
+        firstLabel.text = "Ndryshimi i Labeles"
+        
+    }
+}
+
---
+++ CacttusEdu/SecondViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SecondViewController.swift
+//  CacttusEdu
+//
+//  Created by Gent Berani on 2/14/18.
+//  Copyright © 2018 CacttusEdu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    var test: String = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Greetings from: \(test)")
+        
+    }
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+}
