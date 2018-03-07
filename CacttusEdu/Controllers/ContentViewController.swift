cf3ea7dca7463887c552013fe9f942749fc68dc2
Gent Berani <gentberani@gmail.com>
2018-03-07 20:59:57 +0100

    add: stackview, image assets

---
+++ CacttusEdu/Controllers/ContentViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ContentViewController.swift
+//  CacttusEdu
+//
+//  Created by Gent Berani on 3/7/18.
+//  Copyright © 2018 CacttusEdu. All rights reserved.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
