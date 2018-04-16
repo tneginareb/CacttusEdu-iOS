efd5196997f1829564e723cf1a448b61310e5021
Gent Berani <gentberani@Gents-MacBook-Pro.local>
2018-04-16 20:46:30 +0200

    add qytetet array

--- CacttusEdu/Architecture/Controllers/QytetetViewController.swift
+++ CacttusEdu/Architecture/Controllers/QytetetViewController.swift
@@ -12,6 +12,8 @@ class QytetetViewController: UIViewController, UITableViewDelegate, UITableViewD
 
     @IBOutlet weak var tableView: UITableView!
     
+    lazy var qytetet: [Qytet] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,19 +21,43 @@ class QytetetViewController: UIViewController, UITableViewDelegate, UITableViewD
         tableView.dataSource = self
     }
     
+    private func fillQytetetArray(){
+        let prishtina = Qytet.init(id: 1, name: "Prishtina")
+        qytetet.append(prishtina)
+        
+        let lipjani = Qytet.init(id: 2, name: "Lipjan City")
+        qytetet.append(lipjani)
+        
+        let ferizaj = Qytet.init(id: 3, name: "Ferizaj")
+        qytetet.append(ferizaj)
+        
+        let podujeva = Qytet.init(id: 4, name: "Podujeva")
+        qytetet.append(podujeva)
+        
+        let tirana = Qytet.init(id: 5, name: "Tirana")
+        qytetet.append(tirana)
+    }
+    
+    @IBAction func addPressed(_ sender: UIButton) {
+        fillQytetetArray()
+        tableView.reloadData()
+    }
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 12
+        return qytetet.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        let qyteti = qytetet[indexPath.row]
         
         let titleLabel: UILabel = cell.viewWithTag(10) as! UILabel
-        titleLabel.text = "Hello from position :\(indexPath.row)"
+        titleLabel.text = "\(qyteti.name)"
         
         return cell
     }
---
+++ CacttusEdu/Architecture/Models/Qytet.swift
@@ -0,0 +1,21 @@
+//
+//  Qytet.swift
+//  CacttusEdu
+//
+//  Created by Gent Berani on 4/16/18.
+//  Copyright © 2018 CacttusEdu. All rights reserved.
+//
+
+import UIKit
+
+class Qytet: NSObject {
+
+    let id: Int
+    let name: String
+    
+    init(id: Int, name: String) {
+        self.id = id
+        self.name = name
+    }
+    
+}
